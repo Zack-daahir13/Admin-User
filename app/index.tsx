0517ed344179ad0d77fa143b89430e6cbc7aa7d3
Axmdstar <85846658+Axmdstar@users.noreply.github.com>
2024-11-29 20:04:37 +0300

    first setup

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+import React from "react";
+import {Text} from "react-native-paper";
+
+
+const HomeDemo = () => {
+  return (
+    <View>
+      <Text variant="displayLarge">HomeDemo</Text>
+    </View>
+  );
+};
+
+export default HomeDemo;
