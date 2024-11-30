a44ef9a42a2508f6d189c8aa7881f79ce83b7ce9
malik <malikyakub@hotmail.com>
2024-11-30 21:31:17 +0300

    created a deep linking to phone app with parameter

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,20 @@
-import { View } from "react-native";
+import { Alert, Button, Linking, View } from "react-native";
 import React from "react";
 import {Text} from "react-native-paper";
 
-
 const HomeDemo = () => {
+  function callMe(){
+    const MyPhone = 'tel:*712*613673734*1#' //😉
+    try{
+      Linking.openURL(MyPhone)
+    }catch(error){
+      Alert.alert('ERROR HAPPENED', `${error}`)
+    }
+  }
   return (
     <View>
       <Text variant="displayLarge">HomeDemo</Text>
+      <Button title="Call me" onPress={() => callMe()}/>
     </View>
   );
 };
